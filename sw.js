2fba219f596476624581d5a11ab19d7e0be41675
dark8hw-hash <dark8hw@gmail.com>
2026-02-22 00:46:11 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="planner-v1-2-cache";
-const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./sw.js","./icons/icon-192.png","./icons/icon-512.png","./icons/apple-touch-icon.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))));self.clients.claim();});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).catch(()=>cached)));});
